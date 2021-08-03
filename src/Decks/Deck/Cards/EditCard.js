e0576c5bd6f192d574e1604c5d16098dfb3a243b
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-03 10:21:02 -0500

    Created skeleton for Forms

--- src/Decks/Deck/Cards/EditCard.js
+++ src/Decks/Deck/Cards/EditCard.js
@@ -1,23 +1,85 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
 import Breadcrumb from "../../../CommonComponents/Breadcrumb";
 import LoadingMessage from "../../../CommonComponents/LoadingMessage";
 function EditCard({ deck, cards }) {
   const { cardId } = useParams();
+  const history = useHistory();
+  const card = deck?.cards?.[cardId];
 
-  if (deck?.id && cardId && cards) {
-    const card = deck.cards[cardId];
+  const defaultFormState = { front: "", back: "" };
+  const [formData, setFormData] = useState(defaultFormState);
 
-    return (
-      <>
-        <Breadcrumb navTitles={[deck.name, `Edit Card ${cardId}`]} />
-        <h1>
-          Editing card {card.id} from {deck.name}
-        </h1>
-      </>
+  const formChangeHandler = ({ target: { name, value } }) => {
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(
+      "API Call goes here!\nfor now here's the data:\n",
+      `Front: ${formData.front}\n`,
+      `Back: ${formData.back}`
     );
-  }
+    setFormData(defaultFormState);
+    history.push(""); //This needs to change to deckView
+  };
 
-  return <LoadingMessage />;
+  const cancelHandler = () => {
+    setFormData(defaultFormState);
+    history.push(""); //this needs to change to deckView
+  };
+
+  return deck?.id && cardId && cards ? (
+    <>
+      <Breadcrumb navTitles={[deck.name, `Edit Card ${cardId}`]} />
+      <h1>Edit Card</h1>
+      <form onSubmit={submitHandler}>
+        <div className="form-group">
+          <label htmlFor="front">Front</label>
+          <input
+            className="form-control"
+            id="front"
+            name="front"
+            placeholder="What question will be asked?"
+            title="please fill out the front of the card."
+            value={formData.front}
+            onChange={formChangeHandler}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="back">Back</label>
+          <textarea
+            className="form-control"
+            id="back"
+            name="back"
+            placeholder="What is the answer to that question?"
+            title="Please fill out the back of the card."
+            value={formData.back}
+            onChange={formChangeHandler}
+            required
+          />
+        </div>
+        <div>
+          <button
+            className="btn btn-secondary mr-2"
+            type="button"
+            onClick={cancelHandler}
+          >
+            Cancel
+          </button>
+          <button className="btn btn-primary mr-2" type="submit">
+            Save
+          </button>
+        </div>
+      </form>
+    </>
+  ) : (
+    <LoadingMessage />
+  );
 }
 export default EditCard;
