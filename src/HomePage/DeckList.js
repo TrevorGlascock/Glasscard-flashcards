ef2c0bbeef226391b07f26e5277170a68c7aae76
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-03 14:53:44 -0500

    Restructured HomePage and DeckList to be make the Create Deck Button a part of the DeckList, and have the DeckList also have a LoadingMessage when it isn't fully rendered

--- src/HomePage/DeckList.js
+++ src/HomePage/DeckList.js
@@ -1,30 +1,21 @@
 import React, { useEffect, useState } from "react";
+import CreateDeckBtn from "../CommonComponents/Buttons/CreateDeckBtn";
 import LoadingMessage from "../CommonComponents/LoadingMessage";
-import { listDecks } from "../utils/api";
 import DeckListItem from "./DeckListItem";
 
-function DeckList() {
-  //cards is a state variable array of each deck in the API
-  const [decks, setDecks] = useState([]);
-
+function DeckList({ decks, setDecks }) {
   //map through every deck in decks to create a DeckListItem for each.
   const deckList = decks.map((deck, key) => (
     <DeckListItem key={key} deck={deck} setDecks={setDecks} />
   ));
-
-  //Loads the list of Decks from the API on startup
-  useEffect(() => {
-    const controller = new AbortController(); //to abort old requests
-
-    //API call to {API_BASE_URL}/decks?_embed=cards (All cards embedded in the deck)
-    listDecks(controller.signal)
-      .then(setDecks)
-      .catch((error) => {
-        if (error.name !== "AbortError") throw error;
-      });
-  }, []);
-
-  return <>{deckList}</>;
+  return deckList?.length ? (
+    <>
+      <CreateDeckBtn />
+      {deckList}
+    </>
+  ) : (
+    <LoadingMessage />
+  );
 }
 
 export default DeckList;
--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -1,16 +1,24 @@
-import React from "react";
-import { useRouteMatch } from "react-router-dom";
-import CreateDeckBtn from "../CommonComponents/Buttons/CreateDeckBtn";
+import React, { useEffect, useState } from "react";
+import { listDecks } from "../utils/api";
 import DeckList from "./DeckList";
 
 function HomePage() {
-  const { url } = useRouteMatch();
-  return (
-    <>
-      <CreateDeckBtn />
-      <DeckList />
-    </>
-  );
+  //decks is a state variable array of each deck in the API
+  const [decks, setDecks] = useState([]);
+
+  //Loads the list of Decks from the API on startup
+  useEffect(() => {
+    const controller = new AbortController(); //to abort old requests
+
+    //API call to {API_BASE_URL}/decks?_embed=cards (All cards embedded in the deck)
+    listDecks(controller.signal)
+      .then(setDecks)
+      .catch((error) => {
+        if (error.name !== "AbortError") throw error;
+      });
+  }, []);
+
+  return <DeckList decks={decks} setDecks={setDecks} />;
 }
 
 export default HomePage;
