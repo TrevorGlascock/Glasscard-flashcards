8838f0fd20a000bad0ad598b2972d5caa129fb45
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-03 15:37:53 -0500

    Added functionless navbuttons to the StudyCard template

--- src/Decks/Deck/StudyCard.js
+++ src/Decks/Deck/StudyCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function StudyCard() {
   return (
@@ -6,6 +6,8 @@ function StudyCard() {
       <div className="card-body">
         <h2 className="card-title">Card 1 of 3</h2>
         <p className="card-text">Card text goes here</p>
+        <button className="btn btn-secondary mr-2">Flip</button>
+        <button className="btn btn-primary mr-2">Next</button>
       </div>
     </div>
   );
