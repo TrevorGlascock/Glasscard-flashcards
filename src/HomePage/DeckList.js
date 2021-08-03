31e358e0302032cbd35b1a9ea5b415ff2cce674f
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-03 16:48:32 -0500

    Cleaned up unused React imports

--- src/HomePage/DeckList.js
+++ src/HomePage/DeckList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import CreateDeckBtn from "../CommonComponents/Buttons/CreateDeckBtn";
 import LoadingMessage from "../CommonComponents/LoadingMessage";
 import DeckListItem from "./DeckListItem";
