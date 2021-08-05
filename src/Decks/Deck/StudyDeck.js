d4176b5bc532c1b65b5b0f969a6c5e18d9f2d4a4
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-05 16:32:57 -0500

    Making API call to loadCards in StudyDeck

--- src/Decks/Deck/StudyDeck.js
+++ src/Decks/Deck/StudyDeck.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Breadcrumb from "../../CommonComponents/Breadcrumb";
 import LoadingMessage from "../../CommonComponents/LoadingMessage";
+import { listCards } from "../../utils/api";
 import NotEnoughCards from "./NotEnoughCards";
 import StudyCard from "./StudyCard";
 
 function StudyDeck({ deck }) {
-  return deck?.name && deck?.cards?.length ? (
+  const [cards, setCards] = useState([]);
+
+  //Loads the list of cards in current deck from the API on startup
+  useEffect(() => {
+    const controller = new AbortController(); //to abort old requests
+
+    //API call to {API_BASE_URL}/decks/deckId/?_embed=cards (All cards embedded in the deck)
+    async function loadCards() {
+      listCards(deck?.id, controller.signal)
+        .then(setCards)
+        .catch((error) => {
+          if (error.name !== "AbortError") throw error;
+        });
+    }
+
+    loadCards();
+    return () => controller.abort(); //cleanup
+  }, [deck]);
+
+  return cards && cards?.length ? (
     <>
-      <Breadcrumb navTitles={[deck.name, "Study"]} />
-      <h1>Study: {deck.name}</h1>
-      {deck.cards.length < 3 ? (
-        <NotEnoughCards cards={deck.cards} />
+      <Breadcrumb navTitles={[deck?.name, "Study"]} />
+      <h1>Study: {deck?.name}</h1>
+      {cards.length < 3 ? (
+        <NotEnoughCards cards={cards} />
       ) : (
-        <StudyCard cards={deck.cards} />
+        <StudyCard cards={cards} />
       )}
     </>
   ) : (
