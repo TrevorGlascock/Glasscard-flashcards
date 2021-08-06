8474f97bdb44fbc95e04afa7158bbbeb7b9fd2c5
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-06 15:57:50 -0500

    Final personalized touch added to header text

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -4,8 +4,10 @@ function Header() {
   return (
     <header className="jumbotron tg-bg-dark">
       <div className="container title">
-        <h1 className="display-4">Flashcard-o-matic</h1>
-        <p className="lead">Discover The Flashcard Difference.</p>
+        <h1 className="display-4">Glasscard Flashcards</h1>
+        <p className="lead">
+          A React Flashcard App Developed by Trevor Glascock.
+        </p>
       </div>
     </header>
   );
