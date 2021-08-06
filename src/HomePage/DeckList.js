23374049f26d35c1161c6692188d4717d3a579ab
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-06 10:55:54 -0500

    Removed redundant container

--- src/HomePage/DeckList.js
+++ src/HomePage/DeckList.js
@@ -19,14 +19,14 @@ function DeckList({ decks, setDecks }) {
 
   //If there is a deckList, and it has a non-zero length, render it, otherwise show LoadingMessage
   return deckList?.length ? (
-    <div className="container">
+    <>
       <div className="row">
         <CreateDeckBtn />
       </div>
       <div className="row">
         <div className="list-group">{deckList}</div>
       </div>
-    </div>
+    </>
   ) : (
     <LoadingMessage />
   );
