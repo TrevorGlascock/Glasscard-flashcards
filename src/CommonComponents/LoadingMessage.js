4203e4d2d93831bc5ecffef3270512ea43800bd7
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-07 15:22:04 -0500

    Changed method of CSS import and made loading spinner more consistent with color scheme

--- src/CommonComponents/LoadingMessage.js
+++ src/CommonComponents/LoadingMessage.js
@@ -2,7 +2,7 @@ import React from "react";
 function LoadingMessage() {
   return (
     <div className="text-center">
-      <div className="spinner-border text-primary text-center" role="status">
+      <div className="spinner-border text-info text-center" role="status">
         <span className="sr-only">Now Loading...</span>
       </div>
     </div>
